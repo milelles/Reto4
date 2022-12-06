b085d312b7fe43e35d6bf078489f55951631600a
Giuss1301 <115802496+Giuss1301@users.noreply.github.com>
2022-12-06 15:57:04 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import 'controlador/controladorGeneral.dart';
 import 'interfaz/home.dart';
 
 void main() {
+  Get.put(controladorGeneral());
   runApp(const MyApp());
 }
