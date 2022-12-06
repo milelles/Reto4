6d57de8a0306f317e9f616a5ef7d89b37d808e1a
Giuss1301 <115802496+Giuss1301@users.noreply.github.com>
2022-12-06 15:54:55 -0500

    Add files via upload
    Avance parte 2

--- lib/interfaz/home.dart
+++ lib/interfaz/home.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:reto4/controlador/controladorGeneral.dart';
+import 'package:reto4/interfaz/listar.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+import '../procesos/peticiones.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Geolocalizador',
       theme: ThemeData(
@@ -26,11 +32,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  controladorGeneral Control = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: listar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Alert(
+                  title: "ATENCIÓN!!",
+                  desc: "Esta seguro que desea almacenar su ubicación?",
+                  type: AlertType.info,
+                  buttons: [
+                    DialogButton(
+                        color: Colors.green,
+                        child: Text("SI"),
+                        onPressed: () {
+                          peticionesDB.GuardarPosicion(
+                              "321564,6542586", DateTime.now().toString());
+                          Control.CargarTodaBD();
+                          Navigator.pop(context);
+                        }),
+                    DialogButton(
+                        color: Colors.red,
+                        child: Text("NO"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        })
+                  ],
+                  context: context)
+              .show();
+        },
+        child: Icon(Icons.location_on_outlined),
       ),
     );
   }
