e79beb38afc36b15470b5157da4f4307dbcc7026
Giuss1301 <115802496+Giuss1301@users.noreply.github.com>
2022-12-06 15:55:55 -0500

    Add files via upload

---
+++ lib/controlador/controladorGeneral.dart
@@ -0,0 +1,31 @@
+import 'dart:core';
+
+import 'package:get/get.dart';
+import 'package:reto4/procesos/peticiones.dart';
+
+class controladorGeneral extends GetxController {
+  final Rxn<List<Map<String, dynamic>>> _listaPosiciones =
+      Rxn<List<Map<String, dynamic>>>();
+  final _unaPosicion = "".obs;
+
+/////////////////////
+  void cargaUnaPosicion(String X) {
+    _unaPosicion.value = X;
+  }
+
+  String get unaPosicion => _unaPosicion.value;
+/////////////////////
+
+  void cargaListaPosiciones(List<Map<String, dynamic>> X) {
+    _listaPosiciones.value = X;
+  }
+
+  List<Map<String, dynamic>>? get ListaPosiciones => _listaPosiciones.value;
+
+////////////////////
+
+  Future<void> CargarTodaBD() async {
+    final datos = await peticionesDB.MostrarTodasUbicaciones();
+    cargaListaPosiciones(datos);
+  }
+}
